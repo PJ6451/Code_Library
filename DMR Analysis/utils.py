edf84b417fe67a1db9498a39f1fafb50efafd052
PJ6451 <arcpj6451@gmail.com>
2023-09-14 10:20:58 -0700

    replaced .py with jupyter notebook

---
+++ DMR Analysis/utils.py
@@ -0,0 +1,19 @@
+def get_cols_to_keep():
+    return [
+        "Mine_ID",
+        "MineName",
+        "X",
+        "Y",
+        "Latitude",
+        "Longitude",
+        "ReportYear",
+        "Acres_Dist",
+        "MineStatus",
+        "Operator",
+        "Opt_City",
+        "Owner",
+        "PriProduct",
+        "Other_Prod",
+        "Opt_Type",
+        "FaceAmount",
+    ]
